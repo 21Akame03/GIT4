86f5c9d049636a706301c2bdc91238261668b8ce
Ashitosh Ubdhoot <akame@akame-mac.local>
2024-11-27 09:50:33 +0100

    c4

--- git4c4/src/main/java/com/example/App.java
+++ git4c4/src/main/java/com/example/App.java
@@ -9,28 +9,22 @@ import java.util.*;
 import javafx.geometry.Insets;
 
 public class App extends Application {
-    Set<Integer> LottoNumbers = new TreeSet<>(); // Tree structure is similar to Binary Tree but instead of only 2 branches, it can have more
-    Random random = new Random(955080);
-
-    TextField[] textFields = new TextField[6]; // arrax of text fields
+    // arrax of text fields
        
     @Override
     public void start(Stage stage) {
-
-        // Initialisation of the textfields
-        // empty the Tree set before generating new numbers
-        this.LottoNumbers.clear();
-        // generate new values 
-        while (this.LottoNumbers.size() < 6) {
-            this.LottoNumbers.add(0); // Numbers 1-49
-        }
-        this.populateTextField();
- 
-
+        Random random = new Random(955080);
+        Set<Integer> LottoNumbers = generateNumbers(random); // Tree structure is similar to Binary Tree but instead of only 2 branches, it can have more
+        
+        TextField[] textFields = createTextFields(LottoNumbers);
 
         // New Button to actuate the drawing
         HsdButton drawButton = new HsdButton("Draw");
-        drawButton.setOnAction(event -> Lottoziehung()); // draw new numbers on click hehehehehhe
+        drawButton.setOnAction(event -> {
+            Random newRandom = new Random(955080); 
+            Set<Integer> newNumbers = generateNumbers(newRandom); // Generate new numbers
+            updateTextFields(newNumbers, textFields); // Update UI
+        }); // draw new numbers on click hehehehehhe
 
         VBox root = new VBox(10, textFields);
         root.getChildren().add(drawButton);
@@ -42,27 +36,36 @@ public class App extends Application {
         stage.show();
     }
 
-    // draw new random numbers and show them on the screen.
-    public void Lottoziehung(){
-        // empty the Tree set before generating new numbers
-        this.LottoNumbers.clear();
-        // generate new values 
-        while (this.LottoNumbers.size() < 6) {
-            this.LottoNumbers.add(1 + random.nextInt(49)); // Numbers 1-49
+
+    private Set<Integer> generateNumbers(Random random) {
+        Set<Integer> uniqueNumbers = new TreeSet<>();
+        while (uniqueNumbers.size() < 6) {
+            uniqueNumbers.add(1 + random.nextInt(49)); // between 1 and 49
         }
+        return uniqueNumbers;
+    }
 
-        // then populate the textfields
-        this.populateTextField();
-    } 
-
-    public void populateTextField() {
+    private TextField[] createTextFields(Set<Integer> numbers) {
+        TextField[] textFields = new TextField[6];
         int i = 0;
-        // for every number value in uniquenumbers
-        for (int number : this.LottoNumbers) {
-            this.textFields[i].setText(String.valueOf(number));
+        for (int number : numbers) {
+            textFields[i] = new TextField(String.valueOf(number));
+            textFields[i].setEditable(false);
+            textFields[i].setMinWidth(50);
+            i++;
+        }
+        return textFields;
+    }
+
+    // add the new values to the textfields
+    private void updateTextFields(Set<Integer> LottoNumbers, TextField[] textFields) {
+        int i = 0;
+        for (int number : LottoNumbers) {
+            textFields[i].setText(String.valueOf(number));
             i++;
         }
     }
+
     public static void main(String[] args) {
         launch(args);
     }
